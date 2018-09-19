3d00fffed681aeb02ba00409c69b2faa80ec4de2
atedp <atedplummer@gmail.com>
2018-09-19 19:18:33 -0400

    initial commit w/ a bunch of stuff

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+
+my_name = "ted"
+
+print(f"my name is {my_name}")
---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("your first variable is:", first)
+print("your second variable is:", second)
+print("your thrid variable is:", third)
